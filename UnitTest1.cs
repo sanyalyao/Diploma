56c2086652e24d16fad796adf844f48c247f4c40
sanyalyao <xxxx81971@yandex.ru>
2023-07-06 21:36:19 +0500

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Diploma
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
